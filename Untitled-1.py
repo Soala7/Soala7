6bb4224510e7a80f7ec42e54669e106d9fca84f4
Amachree Soala <nigaamace@gmail.com>
2025-07-12 05:35:22 +0100

    Add files via upload
    simply python generator

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+import random
+while True:
+    s = "a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V", "w", "W", "x", "X", "y", "Y", "z", "Z","@","!","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]",":",";","<",">",",",".","?","/","|","~","#"
+
+    lenght = int(input("Enter the length of the password: "))
+    password = "".join(random.sample(s, lenght))
+    print("Your password is: ", password)
+    choice = input("Do you want to generate another password? (yes/no): ")
+    if choice.lower() != 'yes':
+        print("Thank you for using the password generator!")
+        break
